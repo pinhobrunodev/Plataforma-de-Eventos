d80386fe4e53dd3eb000e36876b2e748e12e0399
pinhobrunodev <iaishowcs@gmail.com>
2022-06-25 13:13:51 -0300

    fix endpoint authservice

--- auth-service/src/main/java/com/pinhobrunodev/plataforma/eventos/authservice/framework/adapters/in/rest/UserController.java
+++ auth-service/src/main/java/com/pinhobrunodev/plataforma/eventos/authservice/framework/adapters/in/rest/UserController.java
@@ -4,6 +4,7 @@ import com.pinhobrunodev.plataforma.eventos.authservice.application.ports.in.Aut
 import com.pinhobrunodev.plataforma.eventos.authservice.domain.dto.request.CreateUserRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class UserController {
     @Autowired
     private AuthServiceUseCase authServiceUseCase;
 
+    @PostMapping(value = "/user/save")
     public ResponseEntity<Void> createUser(@RequestBody CreateUserRequestDTO createUserRequestDTO) {
         authServiceUseCase.createUser(createUserRequestDTO);
         return ResponseEntity.noContent().build();
